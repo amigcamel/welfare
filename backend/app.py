f46c60cdbb25b01916db97cc40ff65aa3fea5821
amigcamel <amigcamel@gmail.com>
2020-05-29 16:48:48 +0800

    chg: dev: major update

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,51 @@
 """App."""
-import os
+import json
 
-from flask import Flask, request, redirect, jsonify, url_for
+from flask import Flask, request, redirect, jsonify, url_for, g
+from flask_cors import CORS
 from loguru import logger
-import jwt
+from jwcrypto import jwe
+from jwcrypto.common import json_encode
 
 from oauth import gen_login_url, get_userinfo
+import settings
 
 app = Flask(__name__)
-SECRET_KEY = os.urandom(24)
-app.config["SECRET_KEY"] = SECRET_KEY
+app.debug = True
+CORS(app)
+app.config["SECRET_KEY"] = settings.APP_SECRET
+
+
+def _decrypt(token):
+    jwetoken = jwe.JWE()
+    jwetoken.deserialize(token)
+    jwetoken.decrypt(settings.JWK_KEY)
+    return json.loads(jwetoken.payload)
+
+
+def _encrypt(data):
+    token = jwe.JWE(data.encode('utf-8'), json_encode({'alg': 'A256KW', 'enc': 'A256CBC-HS512'}))
+    token.add_recipient(settings.JWK_KEY)
+    return token.serialize(compact=True)
 
 
 @app.before_request
 def auth():
     """Authenticate."""
+    logger.debug(request.headers)
+    if request.method == 'OPTIONS':
+        return
     if request.path in (url_for("login"), url_for("login_redirect")):
         return
-    token = request.args.get("token")
+    token = request.headers['Authorization'].split('Bearer')[-1].strip()
     if token:
-        data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        logger.debug(f'token -----> {token}')
+        try:
+            data = _decrypt(token)
+        except Exception as e:
+            logger.critical(e)
+            return 'Unauthorized', 401
+        g.token = token
         logger.debug(data)
     else:
         return "Unauthorized", 401
@@ -33,8 +59,9 @@ def login_redirect():
         return "Code is not provided", 400
     data = get_userinfo(code)
     logger.info(data)
-    token = jwt.encode(data, SECRET_KEY, algorithm="HS256").decode("utf-8")
-    return jsonify({"token": token})
+    token = _encrypt(json.dumps(data))
+    logger.debug(f'token: {token}')
+    return redirect(f'http://localhost:4200/#/home?token={token}')
 
 
 @app.route("/login")
@@ -46,6 +73,30 @@ def login():
 @app.route("/user")
 def user():
     """User info."""
-    token = request.args.get("token")
-    data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+    data = _decrypt(g.token)
     return jsonify(data)
+
+
+@app.route("/hello")
+def hello():
+    """Hello."""
+    logger.debug(request.headers)
+    return '', 200
+
+
+@app.route("/byebye")
+def byebye():
+    """byebye."""
+    return '', 401
+
+
+@app.route('/token')
+def token():
+    """Token."""
+    code = request.args.get("code")
+    if not code:
+        return "Code is not provided", 400
+    data = get_userinfo(code)
+    logger.info(data)
+    token = _encrypt(data)
+    return jsonify({"token": token})
