b512d8f95f95568f0d8cb2ee9240b50c68ada9a1
Hunter Ko <hunterko@HunterdeMacBook-Pro.local>
2020-06-10 14:00:02 +0800

    refactor form data structure

--- frontend/src/app/service/form.service.ts
+++ frontend/src/app/service/form.service.ts
@@ -4,26 +4,24 @@ import { Observable, throwError } from 'rxjs';
 import { AfternoonTeaForm } from '../interface/afternoon-tea-form';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { catchError } from 'rxjs/operators';
-import { tryCatch } from 'rxjs/internal-compatibility';
-
 @Injectable({
   providedIn: 'root'
 })
 export class FormService implements Resolve<AfternoonTeaForm> {
 
-    constructor(private http: HttpClient) {
-    }
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
-        return this.getForm().pipe(catchError((err) => {
-            console.log('load form error:', err);
-            return  throwError(err);
-        }));
-    }
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+      return this.getForm().pipe(catchError((err) => {
+          console.log('load form error:', err);
+          return  throwError(err);
+      }));
+  }
+  constructor(private http: HttpClient) {
+  }
 
-    getForm(): Observable<AfternoonTeaForm> {
-        return  this.http.get<AfternoonTeaForm>('/afternoontea/demo_1');
-    }
-    sendForm(data: any): Observable<any> {
-        return this.http.post('/afternoontea/demo_1', data);
-    }
+  getForm(): Observable<AfternoonTeaForm> {
+      return  this.http.get<AfternoonTeaForm>('/afternoontea/demo_1');
+  }
+  sendForm(data: any): Observable<any> {
+      return this.http.post('/afternoontea/demo_1', data);
+  }
 }
