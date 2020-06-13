1275dd8192f05b72865aa6094c18c748969b36c2
amigcamel <amigcamel@gmail.com>
2020-06-14 01:26:56 +0800

    chg: dev: back up per 6 hour, retain 20 most recent ones.

--- cron/backup.sh
+++ cron/backup.sh
@@ -2,7 +2,7 @@
 BACKUP_DIR='/backup'
 FILENAME='mongobackup'
 NOW=$(date +%Y%m%d%H%M%S)
-RETAIN=10
+RETAIN=20
 /usr/bin/mongodump --host=mongo --archive=${BACKUP_DIR}/${FILENAME}_$NOW.gz --gzip
 # Keep the 10 most recent backups
 ls -t1 $BACKUP_DIR/$FILENAME* | tail -n +$((RETAIN+1)) | xargs rm -f
