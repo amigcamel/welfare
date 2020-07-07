8e206caa8f2b5fcc1c53f2f93509fcc43d5a1a77
amigcamel <amigcamel@gmail.com>
2020-07-07 14:40:33 +0800

    fix: dev: change unauthorized status code to 401

--- backend/backend/exceptions.py
+++ backend/backend/exceptions.py
@@ -35,7 +35,7 @@ class TokenExpiredError(AuthError):
 class UnauthorizedError(AuthError):
     """Handle unauthorized request."""
 
-    errcode = 403
+    errcode = 401
 
 
 class InvalidTokenError(AuthError):
