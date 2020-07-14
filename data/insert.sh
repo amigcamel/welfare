e7066602695b97599e5f4a7856755fffdeff6e39
amigcamel <amigcamel@gmail.com>
2020-07-14 16:05:09 +0800

    new: dev: add script for inserting test dataset

---
+++ data/insert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+dbs=(
+  afternoontea
+  staff
+)
+
+for db in "${dbs[@]}"; do
+  CON=$(docker ps --filter name=welfare_mongo -q)
+  docker cp ./$db.gz $CON:/ 
+  docker exec -t $CON mongorestore -d $db --gzip --drop --archive=$db.gz
+done
