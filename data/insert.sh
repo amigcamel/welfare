d92167bfbf23a860cbafd1744fd9a8808cb858a4
amigcamel <amigcamel@gmail.com>
2020-07-16 09:41:17 +0800

    fix: dev: fix wrong path !minor

--- data/insert.sh
+++ data/insert.sh
@@ -11,5 +11,5 @@ CON=$(docker ps --filter name=welfare_mongo -q)
 docker cp ./mongodemo.gz $CON:/ 
 
 for db in "${dbs[@]}"; do
-  docker exec -t $CON mongorestore --drop --archive=$db.gz --gzip --db=$db
+  docker exec -t $CON mongorestore --drop --archive=mongodemo.gz --gzip --db=$db
 done
